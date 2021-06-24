4632ce68500c66ae51326c5ea6b4ad308d4dc8bd
MauroBraga <M@r202150016774>
2021-06-24 09:43:06 -0300

    primeiro componente

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+export function RepositoryList(){
+    return  (
+        <section className="repository-list">
+            <h1>Lista de Repositório</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
